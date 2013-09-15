5599341cac51630e6f3ee35416e8c3aa1b1e3090
Scott Lin <11ScottL5@gmail.com>
2013-09-14 23:25:26 -0700

    Add article database handler to mark read articles

---
+++ src/com/example/database/ArticleDbAdaptor.java
@@ -0,0 +1,79 @@
+package com.example.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+import com.example.myreader.data.Article;
+
+public class ArticleDbAdaptor {
+	
+	public static final String TABLE_NAME = "entry";
+    public static final String KEY_ROWID = BaseColumns._ID;
+    public static final String KEY_GUID = "guid";
+	public static final String KEY_READ = "read";
+	
+	private MySQLiteHelper sqLiteHelper;
+	private SQLiteDatabase sqLiteDatabase;
+	private Context context;
+	
+	public ArticleDbAdaptor(Context c){
+		context = c;
+	}
+
+	public ArticleDbAdaptor openToRead() throws android.database.SQLException {
+		sqLiteHelper = new MySQLiteHelper(context);
+		sqLiteDatabase = sqLiteHelper.getReadableDatabase();
+		return this; 
+	}
+
+	public ArticleDbAdaptor openToWrite() throws android.database.SQLException {
+		sqLiteHelper = new MySQLiteHelper(context);
+		sqLiteDatabase = sqLiteHelper.getWritableDatabase();
+		return this; 
+	}
+
+	public void close(){
+		sqLiteHelper.close();
+	}
+
+	public long insertBlogListing(String guid) {
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_GUID, guid);
+        initialValues.put(KEY_READ, false);
+        return sqLiteDatabase.insert(TABLE_NAME, null, initialValues);
+    }
+    
+    public Article getBlogListing(String guid) throws SQLException {
+        Cursor mCursor =
+        		sqLiteDatabase.query(true, TABLE_NAME, new String[] {
+                		KEY_ROWID,
+                		KEY_GUID, 
+                		KEY_READ,
+                		}, 
+                		KEY_GUID + "= '" + guid + "'", 
+                		null,
+                		null, 
+                		null, 
+                		null, 
+                		null);
+        if (mCursor != null && mCursor.getCount() > 0) {
+        	mCursor.moveToFirst();
+        	Article a = new Article();
+   			a.setGuid(mCursor.getString(mCursor.getColumnIndex(KEY_GUID)));
+   			a.setRead(mCursor.getInt(mCursor.getColumnIndex(KEY_READ)) > 0);
+   			a.setDbId(mCursor.getLong(mCursor.getColumnIndex(KEY_ROWID)));
+   			return a;
+        }
+        return null;
+    }
+    
+    public boolean markAsRead(String guid) {
+        ContentValues args = new ContentValues();
+        args.put(KEY_READ, true);
+        return sqLiteDatabase.update(TABLE_NAME, args, KEY_GUID + "='" + guid+"'", null) > 0;
+    }
+
+}
