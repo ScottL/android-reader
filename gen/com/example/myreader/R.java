311a65c1e737459c4df3bfbc6fc6fa9cc18dc241
Scott Lin <11ScottL5@gmail.com>
2013-09-30 23:14:34 -0700

    Some ui change

--- gen/com/example/myreader/R.java
+++ gen/com/example/myreader/R.java
@@ -26,7 +26,8 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
         public static final int jbwallpaper=0x7f020003;
         public static final int listbackground=0x7f020004;
-        public static final int tech=0x7f020005;
+        public static final int separator_line=0x7f020005;
+        public static final int tech=0x7f020006;
     }
     public static final class id {
         public static final int ScrollView=0x7f080002;
